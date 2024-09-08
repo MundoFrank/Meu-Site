19224ea56efbf98cdc5a22edc18b8fca10bc8334
MundoFrank <frankcolca@gmail.com>
2024-09-08 16:27:13 -0300

    Update dados.js
    SUBIR OS ARQUIVOS DO MEU SITE

--- dados.js
+++ dados.js
@@ -1,13 +1,18 @@
 let dados = [
+   
     {
-        titulo: "Rir é o melhor remédio: 10 benefícios do riso para a saúde",
-        descricao: "Você com certeza já ouviu dizer que rir é o melhor remédio. Mas será que é verdade mesmo? De acordo com a ciência, sim! E os benefícios vão muito além de melhorar o humor, ajudar a relaxar ou aumentar a autoestima. Afinal de contas, o riso é inato ao ser humano. Até mesmo os bebês, que ainda não sabem como reagir ao mundo à sua volta, sabem rir. E o ato está, na maioria das vezes, associado a sentimentos positivos.",
-        link: "https://blog.portoseguro.com.br/rir-e-o-melhor-remedio-10-beneficios-do-riso-para-a-saude",
-    },
+        titulo: "Tendo um dia ruim? Confira essa seleção de piadas curtas para dar boas risadas!",
+        descricao: "Por que os elétrons nunca são convidados para festas? Porque eles são muito negativos.",
+        link: "https://www.selecoes.com.br/humor/piadas-curtas-e-muito-engracadas/",
+    }, 
     {
         titulo: "Dar risada beneficia a saúde mental e física, além de ajudar na socialização",
         descricao: "Dar risada é considerado benéfico para a saúde, melhorando o humor e diminuindo a intensidade do estresse físico e emocional. Quando rimos, a parte da frente do cérebro, conhecido como lobo frontal, ajuda a interpretar as informações e decidir se são engraçadas.",
         link: "https://capital.sp.gov.br/web/saude/w/noticias/320504",
     },
-
+    {
+        titulo: "Rir é o melhor remédio: 10 benefícios do riso para a saúde",
+        descricao: "Você com certeza já ouviu dizer que rir é o melhor remédio. Mas será que é verdade mesmo? De acordo com a ciência, sim! E os benefícios vão muito além de melhorar o humor, ajudar a relaxar ou aumentar a autoestima. Afinal de contas, o riso é inato ao ser humano. Até mesmo os bebês, que ainda não sabem como reagir ao mundo à sua volta, sabem rir. E o ato está, na maioria das vezes, associado a sentimentos positivos.",
+        link: "https://blog.portoseguro.com.br/rir-e-o-melhor-remedio-10-beneficios-do-riso-para-a-saude",
+    },
     ]
